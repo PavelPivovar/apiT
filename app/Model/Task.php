d08bfbc074a5f1de402013fb109deb338024e445
PavelPivovar <pavel.piv@computools.com>
2018-05-18 16:59:59 +0300

    Relationship created

--- app/Model/Task.php
+++ app/Model/Task.php
@@ -3,8 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+//use App\Model\Task;
 
 class Task extends Model
 {
-    //
+    public function reviews(){
+        return $this->hasMany(Task::class);
+    }
 }

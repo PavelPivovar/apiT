b04ffdc3e0dfcee0c64bf01ac357799c7794e13e
PavelPivovar <pavel.piv@computools.com>
2018-05-24 11:34:59 +0300

    Method update and destroy

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -17,33 +17,61 @@ class ExampleTest extends TestCase
     public function test_get_indel_all()
     {
         $user = factory(User::class)->create();
-        $this->actingAs($user)->get('api/task' , ['title' => 'oinionoi' ])->assertStatus(200);
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ])->actingAs($user)->get('api/task' , [
+            'title' => 'oinionoi'
+        ])->assertStatus(200);
     }
 
     public function test_show_method()
     {
         $task = factory(Task::class)->create();
-        $this->get('/api/task/' . $task->id,  ['body' => 'Sally'])->assertStatus(200);
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ])->get('/api/task/' . $task->id,  [
+            'body' => 'Sally'
+        ])->assertStatus(200);
     }
 
     public function test_store_method()
     {
         $user = factory(User::class)->create();
-        $this->actingAs($user)->json('POST', 'api/task', ['title' => 'heeelll','body' => 'heloo' ])->assertStatus(201);
+        $this->actingAs($user)->withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ])->json('POST', 'api/task', [
+            'title' => 'heeelll',
+            'body' => 'heloo'
+        ])->assertStatus(201);
     }
 
     public function test_delete_method()
     {
         $task = factory(Task::class)->create();
         $user = factory(User::class)->create();
-        $this->actingAs($user)->json('DELETE', 'api/task/' . $task->id, ['title' => 'gtmgt', 'body' => 'gfnff'])->assertStatus(204);
+        $this->actingAs($user)->withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ])->json('DELETE', 'api/task/' . $task->id, [
+            'title' => 'gtmgt',
+            'body' => 'gfnff'
+        ])->assertStatus(204);
     }
 
     public function test_update_method()
     {
         $task = factory(Task::class)->create();
         $user = factory(User::class)->create();
-        $this->actingAs($user)->json('PUT', 'api/task/' . $task->id, ['title' => 'dsdsdsd', 'body' => 'dsdsdsd'])->assertStatus(201);
+        $this->actingAs($user)->withHeaders([
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+        ])->json('PUT', 'api/task/' . $task->id, [
+            'title' => 'dsdsdsd',
+            'body' => 'dsdsdsd'
+        ])->assertStatus(201);
     }
 
 }

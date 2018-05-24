402aba37b422cdfa017deaaf03bc8bdaba65d787
PavelPivovar <pavel.piv@computools.com>
2018-05-24 10:26:04 +0300

    Method update and destroy

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
+use App\Model\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
@@ -12,8 +14,36 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_get_indel_all()
     {
-        $this->assertTrue(true);
+        $user = factory(User::class)->create();
+        $this->actingAs($user)->get('api/task' , ['title' => 'oinionoi' ])->assertStatus(200);
     }
+
+    public function test_show_method()
+    {
+        $task = factory(Task::class)->create();
+        $this->get('/api/task/' . $task->id,  ['body' => 'Sally'])->assertStatus(200);
+    }
+
+    public function test_store_method()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user)->json('POST', 'api/task', ['title' => 'heeelll','body' => 'heloo' ])->assertStatus(201);
+    }
+
+    public function test_delete_method()
+    {
+        $task = factory(Task::class)->create();
+        $user = factory(User::class)->create();
+        $this->actingAs($user)->json('DELETE', 'api/task/' . $task->id, ['title' => 'gtmgt', 'body' => 'gfnff'])->assertStatus(204);
+    }
+
+    public function test_update_method()
+    {
+        $task = factory(Task::class)->create();
+        $user = factory(User::class)->create();
+        $this->actingAs($user)->json('PUT', 'api/task/' . $task->id, ['title' => 'dsdsdsd', 'body' => 'dsdsdsd'])->assertStatus(201);
+    }
+
 }
